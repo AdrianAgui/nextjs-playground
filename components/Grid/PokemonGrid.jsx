e4cf46853087604f0f6bdb412e9e6c88639b92ed
AdriAgui <adrian.agui9@gmail.com>
2022-04-15 19:30:50 +0200

    improvement

--- components/Grid/PokemonGrid.jsx
+++ components/Grid/PokemonGrid.jsx
@@ -1,16 +1,21 @@
+import { useState, useEffect } from 'react';
+
 import PokemonItem from './PokemonItem';
 import PokemonSkeleton from './PokemonSkeleton';
 
 import css from 'styles/PokemonGrid.module.css';
 
 export default function PokemonGrid({ pokemons }) {
-  const dataLoaded = pokemons && pokemons[0] && pokemons.length > 0;
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  useEffect(() => {
+    setDataLoaded(pokemons && pokemons[0] && pokemons.length > 0);
+  }, [pokemons]);
+
   return (
     <div className={css.container}>
       {!dataLoaded && pokemons.map((_, index) => <PokemonSkeleton key={index} />)}
-
       {dataLoaded && pokemons.length > 0 && pokemons.map((pokemon, index) => <PokemonItem pokemon={pokemon} key={index} />)}
-
       {!pokemons && <h1>No hay pokemons</h1>}
     </div>
   );
