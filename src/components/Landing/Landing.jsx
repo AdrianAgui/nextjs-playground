d3fbb456fb503f5e845ec4ce254b0edca3137b62
AdriAgui <adrian.agui9@gmail.com>
2022-04-20 00:39:10 +0200

    fixes

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -12,7 +12,7 @@ export default function Landing() {
 
   return (
     <>
-      <Container>
+      <Container maxW='container.sm'>
         <Box borderRadius='md' px='4' py='4' bg='blue.600' maxW='lg' align='center'>
           <Text className='w-25' as='span' fontSize='16px' color='white' align='center'>
             Welcome <b>{user ? user.name : 'Username'}</b> to PokeGen, app created with <b>NextJS</b>.
