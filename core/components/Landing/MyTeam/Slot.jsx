5c381afd9f7790311050eef5c56089b40b588e80
AdriAgui <adrian.agui9@gmail.com>
2022-04-25 23:20:41 +0200

    slot shadows

--- core/components/Landing/MyTeam/Slot.jsx
+++ core/components/Landing/MyTeam/Slot.jsx
@@ -5,13 +5,14 @@ import css from 'styles/MyTeam.module.scss';
 
 function Slot({ pokemon }) {
   return (
-    <div key={pokemon.id} className=''>
-      <div className='relative'>
+    <div key={pokemon.id} className='relative'>
+      <div className={pokemon.id && css.teamMate}>
         {pokemon.id ? (
           <Image src={pokemon.imageURL} alt={pokemon.name} className='z-10' width={150} height={150} />
         ) : (
-          <div style={{ width: '150px', height: '156px' }} />
+          <div style={{ width: '150px', height: '150px' }} />
         )}
+
         <div className='absolute top-0 left-0'>
           <div className={css.pokeball}></div>
         </div>
