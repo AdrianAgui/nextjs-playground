f909e0c4e743c4f46ab70e7280f5a49edda89ef2
AdriAgui <adrian.agui9@gmail.com>
2022-05-24 21:10:02 +0200

    navigate to static paths

--- core/components/Header/LangsSelector.jsx
+++ core/components/Header/LangsSelector.jsx
@@ -5,18 +5,24 @@ import { useI18n } from 'core/context/i18nContext';
 
 import SpanishFlag from './../Icons/Flags/Spanish';
 import EnglishFlag from './../Icons/Flags/English';
+import { useEffect, useState } from 'react';
 
 export default function LangsSelector() {
-  const { pathname, locales } = useRouter();
+  const { pathname, locales, query } = useRouter();
   const { translator } = useI18n();
+  const [path, setPath] = useState(pathname);
+
+  useEffect(() => {
+    !pathname.includes('[id]') ? setPath(pathname) : setPath(pathname.replace('[id]', query.id));
+  }, []);
 
   return (
     <nav className='ml-14'>
       <ul className='flex'>
         {locales.map((locale) => {
           return (
-            <li id={locale} key={locale} className='ml-1' title={translator(`language.${locale}`)}>
-              <Link locale={locale} href={pathname}>
+            <li id={`${locale}-lang-changer`} key={locale} title={translator(`language.${locale}`)} className='ml-1'>
+              <Link locale={locale} href={path}>
                 <a>{locale === 'es' ? <SpanishFlag /> : <EnglishFlag />}</a>
               </Link>
             </li>
